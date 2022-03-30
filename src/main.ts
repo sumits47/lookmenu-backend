72e92c98e0e7f8618c8418b2cf95cbd4d768a15a
Sumit Sen <sumit@enlightyx.io>
2022-03-30 17:26:20 +0800

    feat: added .env based configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger(AppModule.name);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const cfg = app.get(ConfigService);
+  const port = cfg.get<number>('port');
+  await app.listen(port);
+  logger.log(`Application started on port - ${port}`);
 }
 bootstrap();
